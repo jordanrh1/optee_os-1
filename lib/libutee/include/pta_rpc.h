1e14833949bf23a098c53352781754d24687e10a
Jordan Rhee <jordanrh@microsoft.com>
2018-05-14 09:41:49 -0700

    PTA_RPC initial implementation

---
+++ lib/libutee/include/pta_rpc.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (C) Microsoft. All rights reserved.
+ */
+
+#ifndef __PTA_RPC_H__
+#define __PTA_RPC_H__
+
+/* PTA UUID: {ad0fd0ae-09e1-464b-98ed-0607ec9ebd8b} */
+#define PTA_RPC_UUID { 0xad0fd0ae, 0x09e1, 0x464b, { \
+                       0x98, 0xed, 0x06, 0x07, 0xec, 0x9e, 0xbd, 0x8b } }
+
+/* Command IDs supported by the RPC PTA */
+enum PTA_RPC_COMMANDS
+{
+    PTA_RPC_EXECUTE,
+};
+
+#endif /* __PTA_RPC_H__ */
